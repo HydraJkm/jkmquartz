b17e7fdb2d876ba9506b15e334d1ad18909dd491
hydrajkm <wildworldforeverwow@gmail.com>
2025-07-07 11:24:33 -0600

    Quartz sync: Jul 7, 2025, 11:24 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Margarine",
+        header: "Fauna One",
         body: "Roboto Mono",
         code: "IBM Plex Mono",
       },
